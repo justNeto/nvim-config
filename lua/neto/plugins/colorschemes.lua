05a6ebbad057eefcabfad4bc46e622110e941beb
justNeto <continental.lelgg.2b@gmail.com>
2025-03-08 23:36:22 -0600

    Testing new theme

--- lua/neto/plugins/colorschemes.lua
+++ lua/neto/plugins/colorschemes.lua
@@ -1,37 +1,29 @@
 return
 {
+	-- {
+	-- 	'nyoom-engineering/oxocarbon.nvim',
+	-- 	lazy 		= false,
+	-- 	priority 	= 1000,
+	-- 	config 		= function()
+	-- 		vim.cmd([[colorscheme oxocarbon]])
+ --            vim.opt.background = "dark" -- set this to dark or light
+	-- 	end,
+	-- },
+	-- {
+	-- 	'EdenEast/nightfox.nvim',
+	-- 	lazy 		= false,
+	-- 	priority 	= 1000,
+	-- 	config 		= function()
+	-- 		vim.cmd([[colorscheme nightfox]])
+	-- 	end,
+	-- }
+
 	{
-		'fcpg/vim-fahrenheit',
+		'dasupradyumna/midnight.nvim',
 		lazy 		= false,
 		priority 	= 1000,
 		config 		= function()
-			vim.cmd([[colorscheme fahrenheit]])
+			vim.cmd([[colorscheme midnight]])
 		end,
-	},
-	-- {
- --        "savq/melange-nvim",
-	-- 	lazy 		= false,
-	-- 	priority 	= 1000,
- --        config      = function()
- --            vim.cmd([[colorscheme melange]])
- --        end
-	-- },
-    -- INFO: LOOKS GOOD!
-	-- {
- --        "Love-Pengy/lillilac.nvim",
-	-- 	lazy 		= false,
-	-- 	priority 	= 1000,
- --        config      = function()
- --            vim.cmd([[colorscheme lillilac]])
- --        end
- --    },
-    -- TEST: TRY
-	-- {
- --        "bartekprtc/distinct.nvim",
-	-- 	lazy 		= false,
-	-- 	priority 	= 1000,
- --        config      = function()
- --            vim.cmd([[colorscheme distinct]])
- --        end
-	-- },
+	}
 }
