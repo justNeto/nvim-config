7175c9589ec1b9b27c8b788661566f8a85920ee2
justNeto <continental.lelgg.2b@gmail.com>
2025-05-16 15:42:51 +0000

    Fixed nvim config. TODO: add handlers for lsps

--- lua/neto/core/keymaps.lua
+++ lua/neto/core/keymaps.lua
@@ -10,6 +10,9 @@ keymap.set("n", "<leader>sr", "<cmd>FzfLua registers<cr>")
 keymap.set("n", "<leader>sw", "<cmd>FzfLua grep_cword<cr>")
 keymap.set("n", "<leader><space>", "<cmd>FzfLua live_grep<cr>")
 
+keymap.set("n", "<leader>ecd", "lua local current_dir = vim.fn.getcwd(); FzfLua.live_grep({ cwd = current_dir })<cr>")
+keymap.set("n", "<leader>ed", "lua FzfLua.live_grep({ cwd = ''})<left><left><left>")
+
 -- Todo comments
 keymap.set("n", "]t", "<cmd>lua require('todo-comments').jump_next()<cr>")
 keymap.set("n", "[t", "<cmd>lua require('todo-comments').jump_prev()<cr>")
