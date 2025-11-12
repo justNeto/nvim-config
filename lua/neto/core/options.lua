9ffbe7e895897ad0799844c9aaac813df51b22e4
justNeto <continental.lelgg.2b@gmail.com>
2025-11-11 20:44:08 -0600

    Removed ufo + sane folds

--- lua/neto/core/options.lua
+++ lua/neto/core/options.lua
@@ -18,10 +18,13 @@ opt.wrap                 = false
 opt.backspace            = "indent,eol,start"
 
 -- Fold settings
-vim.o.foldcolumn = '0' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.foldlevel = 99
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = "-"
+vim.opt.foldcolumn = "0"
+-- vim.opt.fillchars:append({fold = " "})
 
 -- Search settings
 opt.ignorecase           = true
